f7e2d00731449264d8792f1e11c5f10e8b02db21
Benjamin5774 <1344144091@qq.com>
2025-10-18 16:23:51 -0600

    projectStartUp

---
+++ Source/LunchMeow/LunchMeowGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LunchMeowGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALunchMeowGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALunchMeowGameMode();
+};
+
+
+

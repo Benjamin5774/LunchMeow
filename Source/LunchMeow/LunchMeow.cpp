fddeee9d7cfb27e450a45621dae13ee712896730
Benjamin5774 <1344144091@qq.com>
2025-10-18 21:17:25 -0600

    change project to BP

--- Source/LunchMeow/LunchMeow.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "LunchMeow.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LunchMeow, "LunchMeow" );
-
-DEFINE_LOG_CATEGORY(LogLunchMeow)
- 
